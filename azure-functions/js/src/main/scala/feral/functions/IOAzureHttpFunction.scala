e351fa880ebac0980265eec1f2e0ef2455b971a1
Tim <tim@witandwhimsey.org>
2026-06-28 10:01:19 -0400

    fixed function name

--- azure-functions/js/src/main/scala/feral/functions/IOAzureHttpFunction.scala
+++ azure-functions/js/src/main/scala/feral/functions/IOAzureHttpFunction.scala
@@ -4,27 +4,20 @@ import scala.scalajs.js
 import scala.scalajs.js.annotation._
 //import scala.scalajs.js.JSConverters._
 
-object IOAzureHttpFunction {
-  //case class AppObj(methods: js.Array[String], )
-}
-
 @js.native
 @JSImport("@azure/functions", "app")
 object App extends js.Object {
   def http(
       name: String,
       appConfig: js.Object
-      //handler: js.Function2[js.Any, js.Any, js.Promise[js.UndefOr[js.Any]]]): Unit = js.native
   ): Unit = js.native
 }
 
 abstract class IOAzureHttpFunction {
   final def main(args: Array[String]): Unit =
-    App.http("functionName", appConfig)
+    App.http(functionName, appConfig)
 
-  private lazy val handlerFn: js.Function2[js.Any, js.Any, js.Promise[js.UndefOr[js.Any]]] = {
-    (request, context) => js.Promise.resolve[js.Any](request)
-  }
+  protected val functionName: String = getClass.getSimpleName.init
 
   private val appConfig = js.Dynamic.literal(
     methods = js.Array("GET", "PUT"),
@@ -32,4 +25,12 @@ abstract class IOAzureHttpFunction {
     route = "{*path}",
     handler = handlerFn
   )
+
+  private lazy val handlerFn: js.Function2[js.Any, js.Any, js.Promise[js.UndefOr[js.Any]]] = {
+    (request, context) => js.Promise.resolve[js.Any](context)
+  }
+}
+
+object IOAzureHttpFunction {
+  //case class AppObj(methods: js.Array[String], )
 }
