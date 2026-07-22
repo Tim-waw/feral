f37f11daaec8d59a98b86bf799b8fc5a061e5bdb
Tim <tim@witandwhimsey.org>
2026-07-22 16:37:27 -0400

    switching to dispatcher

--- azure-functions/js/src/main/scala/feral/functions/IOAzureHttpFunction.scala
+++ azure-functions/js/src/main/scala/feral/functions/IOAzureHttpFunction.scala
@@ -7,19 +7,24 @@ import feral.functions.facade.InvocationContext
 import org.http4s.HttpApp
 import cats.effect.IO
 import cats.effect.Resource
+import cats.syntax.all._
+import cats.effect.unsafe.IORuntime
 
 //import scala.scalajs.js.JSConverters._
 import org.http4s.Request
 import feral.functions.facade.JSRequest
 import feral.functions.facade.JSHeaders
+import cats.effect.std.Dispatcher
 
 abstract class IOAzureHttpFunction {
-  def buildHttpApp(context: InvocationContext): Resource[IO, HttpApp[IO]]
+  protected def handler: InvocationContext => Resource[IO, HttpApp[IO]]
+
+  private val runtime = IORuntime.global
 
   final def main(args: Array[String]): Unit =
     IOAzureHttpFunction.App.http(functionName, appConfig)
 
-  protected val functionName: String = getClass.getSimpleName.init //may want to add timestamp for testing
+  private val functionName: String = getClass.getSimpleName.init //may want to add timestamp for testing
 
   private val appConfig = js
     .Dynamic
@@ -31,8 +36,19 @@ abstract class IOAzureHttpFunction {
     )
 
   private lazy val handlerFn
-      : js.Function2[JSRequest, InvocationContext, js.Promise[js.UndefOr[js.Any]]] = {
+    : js.Function2[JSRequest, InvocationContext, js.Promise[js.UndefOr[js.Any]]] = {
+    val dispatcherHandle = {
+      Dispatcher
+        .parallel[IO](await = true)
+        .product(Resource.pure(handler))
+        .allocated
+        .map(_._1) //drop unused finalizer, this resource will live for the duration
+        .unsafeToPromise()(runtime)
+    }
+
     (request, context) => {
+      //do dispatcher thing
+      /////
       val h = request.headers
       val headers = JSHeaders.keyList(h)
       context.log(s"final pipeline! last try for now")
